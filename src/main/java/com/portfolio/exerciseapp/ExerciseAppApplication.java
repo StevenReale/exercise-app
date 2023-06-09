74ef6b1016564b03b0cca385db85cc91daa8993d
Steven Reale <stevenmreale@gmail.com>
2023-06-09 14:11:27 -0400

    Spring Boot initialized and up and running

---
+++ src/main/java/com/portfolio/exerciseapp/ExerciseAppApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.exerciseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/exerciseapp/ExerciseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.exerciseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
