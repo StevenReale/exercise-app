1bfe805c11c17040e0dd7080e0b56adb2bc8b2a6
Steven Reale <stevenmreale@gmail.com>
2023-07-28 13:49:37 -0400

    Added methods for Create, GetById, and GetAll in ExerciseController. Tested in Postman

---
+++ server/src/main/java/com/portfolio/exerciseapp/controller/ExerciseController.java
@@ -0,0 +1,48 @@
+package com.portfolio.exerciseapp.controller;
+
+import com.portfolio.exerciseapp.dao.ExerciseDAO;
+import com.portfolio.exerciseapp.model.Exercise;
+import com.portfolio.exerciseapp.service.ExerciseService;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+@RequestMapping(path = "/exercises")
+public class ExerciseController {
+
+    private ExerciseService exerciseService;
+
+    public ExerciseController(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+    @PreAuthorize("permitAll()")
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Exercise> getAllExercises() {
+        return exerciseService.getAllExercises();
+    }
+
+    @PreAuthorize("permitAll()")
+    @RequestMapping(path = "/{exerciseId}", method = RequestMethod.GET)
+    public Exercise getExerciseById(@PathVariable int exerciseId) {
+        return exerciseService.getExerciseById(exerciseId);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Exercise addExercise(@RequestBody Exercise exercise, Principal principal) {
+        Exercise created = new Exercise();
+        try {
+            created = exerciseService.createExercise(exercise, principal);
+        } catch (java.nio.file.AccessDeniedException e) {
+            e.printStackTrace();
+        }
+        return created;
+    }
+}
+
