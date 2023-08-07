9a0a41231eaee474e1abbe6720e4d523b73238f9
dgress95 <108832982+dgress95@users.noreply.github.com>
2023-08-07 11:37:51 -0500

    Getting views set up and routes working

---
+++ client/src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+         <div class="header-info">
+          <h1>My Logbook</h1>
+         </div>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  header {
+      display: flex;
+      height: 50px;
+      width: 100%;
+      background:black;
+      position: fixed;
+      align-items: center;
+      justify-content: center;
+      top: 0;
+      z-index: 1;
+  }
+  
+  h1 {
+    color: white;
+    margin-bottom: 10px;
+    font-size: 20px;
+  }
+  </style>
---
+++ client/src/router/index.js
@@ -0,0 +1,72 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import LogoutView from '../views/LogoutView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import store from '../store/index'
+
+/* eslint-disable */
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: LogoutView,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterView,
+    meta: {
+      requiresAuth: false
+    }
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  // Determine if the route requires Authentication
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+
+  // If it does and they are not logged in, send the user to "/login"
+  if (requiresAuth && store.state.token === '') {
+    next('/login');
+  } else {
+    // Else let them go to their next destination
+    next();
+  }
+});
+
+export default router
